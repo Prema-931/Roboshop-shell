b80803b34fd25b0e74fdd181589a239d36907442
Prema-931 <premaskht@gmail.com>
2023-12-28 11:53:40 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -48,7 +48,7 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf   &>> $LOGFILE
 
 VALIDATE $? "Remote access to MongoDB"
 
-systemctl restart mongod
+systemctl restart mongod  &>> $LOGFILE
 
 VALIDATE $? "Restarting MongoDB"
 
