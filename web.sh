21879b18a96759928476cdc027f335d901a5c278
Prema-931 <premaskht@gmail.com>
2023-12-28 17:04:37 +0530

    web

--- web.sh
+++ web.sh
@@ -5,12 +5,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-exec &>> $LOGFILE
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started executing at $TIMESTAMP" 
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -30,38 +29,38 @@ else
      echo "You are root user"
 fi  # fi means reverse of if, indicating condition end
 
-dnf install nginx -y  
+dnf install nginx -y    &>> $LOGFILE
 
 VALIDATE $? "Installing nginx"
 
-systemctl enable nginx
+systemctl enable nginx   &>> $LOGFILE
 
 VALIDATE $? "Enable nginx"
 
-systemctl start nginx
+systemctl start nginx    &>> $LOGFILE
 
 VALIDATE $? "starting nginx"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/*   &>> $LOGFILE
 
 VALIDATE $? "Removed default website"
 
-curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip        &>> $LOGFILE
 
 VALIDATE $? "Downloaded web application"
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html       &>> $LOGFILE
 
 VALIDATE $? "moving nginx html directory"
 
-unzip -o /tmp/web.zip
+unzip -o /tmp/web.zip     &>> $LOGFILE
 
 VALIDATE $? "Unzipping web"
 
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf    &>> $LOGFILE
 
 VALIDATE $? "Copied roboshop reverse proxy config"
 
-systemctl restart nginx 
+systemctl restart nginx    &>> $LOGFILE
 
 VALIDATE $? "restarted nginx"
