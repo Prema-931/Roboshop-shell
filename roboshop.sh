b140a5a971c89a0b036bca10b6ef11e39b3284a4
Prema-931 <premaskht@gmail.com>
2023-12-29 15:26:17 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -14,6 +14,6 @@ do
     else
          INSTANCES_TYPE="t2.micro"
     fi   
-     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE t2.micro --security-group-ids sg-0221f74584c684f30
+     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE t2.micro --security-group-ids sg-0221f74584c684f30 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
        
 done 
