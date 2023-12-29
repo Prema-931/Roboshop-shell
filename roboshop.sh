45047fc8f57a7f1908dfe7431f4d6f0354cbdcc3
Prema-931 <premaskht@gmail.com>
2023-12-29 21:16:44 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -12,7 +12,7 @@ do
     then 
          INSTANCE_TYPE="t3.small"
     else
-         INSTANCES_TYPE="t2.micro"
+         INSTANCE_TYPE="t2.micro"
     fi   
      IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE t2.micro --security-group-ids sg-0221f74584c684f30  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
      echo "$i: $IP_ADDRESS"  
