3c725be83caa16701204fe4c9400616e77a65cdf
Prema-931 <premaskht@gmail.com>
2023-12-30 18:22:01 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -15,9 +15,11 @@ do
          INSTANCE_TYPE="t2.micro"
     fi   
      IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0221f74584c684f30  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-     echo "$i: $IP_ADDRESS"  
+     echo "$i: $IP_ADDRESS" 
 
-   #create R53 record, make sure you delete existing record
+done
+
+     #create R53 record, make sure you delete existing record
 aws route53 change-resource-record-sets \
     --hosted-zone-id "$HOSTED_ZONE_ID" \
     --change-batch "{
@@ -33,4 +35,3 @@ aws route53 change-resource-record-sets \
     }"
     echo "record created successfully for $i : $i.$DOMAIN_PATH"
 
-done    
